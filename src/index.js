9627c0912b6fb4f23ffccc67cb488829cdda898b
Selina Zheng <selzheng@iu.edu>
2024-02-15 15:49:52 -0500

    Pushing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors()); 
 
 app.get('/', (req, res) => {
     res.send('Arithmetic service - Hello World');
